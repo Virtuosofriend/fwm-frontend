1288d129c9ccb2b4e9170155f7d5c279f239fb68
Virtuosofriend <dimitris.sakella@gmail.com>
2021-03-06 16:39:54 +0200

    Vuetify added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/no-unused-vars": 0,
-    "vue/no-unused-components": 0,
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+    parserOptions: {
+        parser: "babel-eslint"
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "vue/no-unused-vars": 0,
+        "vue/no-unused-components": 0
+    }
 };
